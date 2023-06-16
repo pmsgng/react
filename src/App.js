814dc8b4236a960e7f108acce970a385483dd44d
Vlad <pmsgng88@gmail.com>
2023-06-16 15:39:40 +0300

    useState()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './Header';
+import MyBtn from './MyBtn';
+import Technologies from './Technologies';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Technologies />
+      <MyBtn />
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p className='header__text'>Hello, world!</p>
+        </div>
+    );
+};
+
+export default Header;
