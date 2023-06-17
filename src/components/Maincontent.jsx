379ba721fa04be85096d8bdac48330c9b832c80c
Vlad <pmsgng88@gmail.com>
2023-06-17 15:25:39 +0300

    placement of site blocks

--- src/components/Maincontent.jsx
+++ src/components/Maincontent.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const Maincontent = () => {
     return (
         <div className='content'>
-            CONTENT
+            <div>
+                <img src="https://phonoteka.org/uploads/posts/2021-04/1619059091_9-phonoteka_org-p-razmitii-fon-nochnogo-goroda-10.jpg" alt="" />
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                    <div>post3</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav className='nav'>
-            NAVBAR
+            <div>
+            <a href="">Profile</a>
+            </div>
+            <div><a href="">Messages</a></div>
+            <div><a href="">News</a></div>
+            <div><a href="">Music</a></div>
+            <div><a href="">Settings</a></div>
         </nav>
     );
 };
