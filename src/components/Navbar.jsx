95dcfb8deb5ba661f16314c5756acd9c2f32e0a7
Vlad <pmsgng88@gmail.com>
2023-06-21 23:16:53 +0300

    navbar styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav className='nav'>
-            <div className='nav__item'><a href="">Profile</a></div>
-            <div className='nav__item'><a href="">Messages</a></div>
-            <div className='nav__item'><a href="">News</a></div>
-            <div className='nav__item'><a href="">Music</a></div>
-            <div className='nav__item'><a href="">Settings</a></div>
+            <div className='nav__item'>
+                <a href=" ">Profile</a>
+            </div>
+            <div className='nav__item'>
+                <a href=" ">Messages</a>
+            </div>
+            <div className='nav__item'>
+                <a href=" ">News</a>
+            </div>
+            <div className='nav__item'>
+                <a href=" ">Music</a>
+            </div>
+            <div className='nav__item'>
+                <a href=" ">Settings</a>
+            </div>
         </nav>
     );
 };
