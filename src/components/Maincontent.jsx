72c19c16d1a4aa782f2ac2c6047a2c6b260b3ed3
Vlad <pmsgng88@gmail.com>
2023-06-22 16:13:19 +0300

    styled content block

--- src/components/Maincontent.jsx
+++ src/components/Maincontent.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Maincontent = () => {
     return (
         <div className='content'>
-            <div>
+            <div className='content__background'>
                 <img src="https://phonoteka.org/uploads/posts/2021-04/1619059091_9-phonoteka_org-p-razmitii-fon-nochnogo-goroda-10.jpg" alt="" />
             </div>
-            <div>
+            <div className='content__user'>
                 ava + descr
             </div>
             <div>
