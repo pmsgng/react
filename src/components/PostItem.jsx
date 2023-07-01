cca4c8eeecfd0b8e87639477b2aa9041a70bd5a6
Vlad <pmsgng88@gmail.com>
2023-07-01 11:53:04 +0300

    useState for component postItem

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/PostItem.css'
 
 const PostItem = ({message,img}) => {
-
+    const[likeCount,setLikeCount] = useState(0)
     return (
         <div className='content__posts-item'>
             <img className='content__posts-item-img' src={img} alt="" />
             <div className="content__posts-item-description">{message}</div>
-            <div className="content__posts-item-like-count">{0}</div>
-            <button className='content__posts-item-like'>♡</button>
+            <div className="content__posts-item-like-count">{likeCount}</div>
+            <button className='content__posts-item-like' onClick={() => setLikeCount(likeCount + 1)}>♡</button>
         </div>
     );
 };
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,7 +4,7 @@ import './css/User.css'
 const User = () => {
     return (
         <div className='content__user'>
-            <img className='content__user-avatar' src="https://cdn.dribbble.com/users/2495677/screenshots/5039870/js-ninja-01.png" alt="" />
+            <img className='content__user-avatar' src="https://avatars.mds.yandex.net/i?id=5ccd4245265bff420a240099860ce351f20ad5d1-8991021-images-thumbs&n=13" alt="" />
             <div className="content__user-description">
                 <div className="content__description-user-name">Kate Larson</div>
                 <div className="content__description-user-birthday">Date of Birth: 3 April</div>
