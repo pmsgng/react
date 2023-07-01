db033322366c3429b1c79878de4489d157f5d504
Vlad <pmsgng88@gmail.com>
2023-07-01 11:46:24 +0300

    useState for input Post and posts.map()

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './css/PostItem.css'
 
-const PostItem = ({message,img,likeCount}) => {
+const PostItem = ({message,img}) => {
+
     return (
         <div className='content__posts-item'>
             <img className='content__posts-item-img' src={img} alt="" />
             <div className="content__posts-item-description">{message}</div>
-            <div className="content__posts-item-like-count">{likeCount}</div>
+            <div className="content__posts-item-like-count">{0}</div>
             <button className='content__posts-item-like'>♡</button>
         </div>
     );
--- src/components/PostsCreator.jsx
+++ src/components/PostsCreator.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostItem from './PostItem';
 import './css/PostsCreator.css'
 
 const PostsCreator = () => {
+    // const [message,setMessage] = useState('')
+    const [inputValue , setInputValue] = useState('')
+    const [posts, setPosts] = useState([])
+
+    function addPost() {
+        // setMessage(inputValue)
+
+        let newPost = {
+            message: inputValue,
+            img: 'https://avatars.mds.yandex.net/i?id=5ccd4245265bff420a240099860ce351f20ad5d1-8991021-images-thumbs&n=13',
+        }
+        setPosts([...posts,newPost])
+        setInputValue('')
+    }
+
+
+
     return (
         <div className='content__post-creator'>
             <div className='content__post-creator-header'>My posts</div>
             <div className='content__post-creator-input-block'>
-                <input className='content__post-creator-input' type="text" />
-                <button className='content__post-creator-btn-add'>Add post</button>
+                <input className='content__post-creator-input' value={inputValue} onChange={(e) => setInputValue(e.target.value)} type="text" />
+                <button className='content__post-creator-btn-add' onClick={() => addPost()}>Add post</button>
                 <button className='content__post-creator-btn-remove'>Remove</button>
             </div>
             <div className='content__post-creator-posts'>
-            <PostItem likeCount={3} img={'https://i.pinimg.com/originals/01/c7/b1/01c7b181419e15cc614b2297a0e0b959.jpg'} message={'Hi! How are u?'}/>
-            <PostItem likeCount={2} img={'https://avatars.mds.yandex.net/i?id=5ccd4245265bff420a240099860ce351f20ad5d1-8991021-images-thumbs&n=13'} message={"It's my first post"}/>
-            <PostItem likeCount={12} img={'https://avatars.mds.yandex.net/i?id=9682272ee70ee163f608d79e79db98e9260562a0-9224110-images-thumbs&n=13'} message={'Do u want to be my friend?'} />    
+                {posts.map((post) => {
+                    return <PostItem img={post.img}  message={post.message} />
+                })}
             </div>
         </div>
     );
